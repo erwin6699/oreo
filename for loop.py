537fa7be7eaa75189c43f275a06b96ba7c2dfe91
evicencio-splunk <evicencio@splunk.com>
2020-01-18 02:09:03 -0800

    Create for loop.py

---
+++ for loop.py
@@ -0,0 +1,14 @@
+# for-in-loop to loop a List in Python
+
+frameworks = ['flask', 'pyramid', 'django'] #list
+
+for temp in frameworks:
+    print temp
+    print "Python framework : %s" %temp
+
+#example 2
+numbers = [2,4,6,8,10] #list
+
+for num in numbers:
+    print num
+    print "Number : %d" %num
